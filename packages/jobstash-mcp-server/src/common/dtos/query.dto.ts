891bac9ac1431c6da1e64adec3df0879496a7bc7
defuera <denis@devartel.io>
2025-04-23 18:19:55 +0200

    Revert "Revert "chore: delete jobstash-mcp-server""
    This reverts commit da178659f840483c1e10f97479849b90287be650.

--- packages/jobstash-mcp-server/src/common/dtos/query.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class QueryDto {
-  @IsString()
-  @IsNotEmpty()
-  query: string;
-} 
--- packages/jobstash-mcp-server/src/parameter-extraction/parameter-extraction.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ParameterExtractionController } from './parameter-extraction.controller';
-import { McpManagerProviderModule } from '../providers';
-
-@Module({
-  imports: [McpManagerProviderModule],
-  controllers: [ParameterExtractionController],
-})
-export class ParameterExtractionModule {}
